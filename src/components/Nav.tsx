6f65f719377fbfb8769f98ae897cdb75877e8709
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-02-24 20:58:10 +0800

    Added links to navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,26 @@
+import Link from "next/link";
 import BaseButton from "./BaseButton";
 
 export default function Nav() {
   return (
-    <div className="flex items-center px-10 py-4">
-      <p className="flex-grow font-bold text-slate-900">Pomogrids</p>
-      <BaseButton
-        type="button"
-        label="Sign In"
-        className="text-blue4 hover:underline underline-offset-2 mr-10"
-      />
-      <BaseButton
-        type="button"
-        label="How to ->"
-        className="text-white bg-blue4 px-4 py-2"
-      />
+    <div className="flex items-center px-10 py-6">
+      <Link href="/" className="flex-grow font-bold text-slate-900">
+        Pomogrids
+      </Link>
+      <Link href="/signin">
+        <BaseButton
+          type="button"
+          label="Sign In"
+          className="text-blue4 hover:underline underline-offset-2 mr-10"
+        />
+      </Link>
+      <Link href="/how-to">
+        <BaseButton
+          type="button"
+          label="How to ->"
+          className="text-white bg-blue4 px-4 py-2"
+        />
+      </Link>
     </div>
   );
 }
