861699efa2416b382a85f08ed4020c39d5c54e12
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-03-13 21:09:06 +0800

    Edits to underConstruction modal

--- src/components/UnderConstruction.tsx
+++ src/components/UnderConstruction.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function UnderConstruction() {
   return (
     <div className="backdrop-blur-sm inset-0 bg-slate-700/20 fixed fade-in z-50">
-      <div className="flex flex-col border border-slate-900 shadow-custom shadow-slate-900 rounded sticky top-28 mx-auto bg-white w-[500px] text-slate-900 p-6">
+      <div className="flex flex-col border border-slate-900 shadow-custom shadow-slate-900 rounded sticky top-28 mx-auto bg-white w-[600px] text-slate-900 p-6">
         <h1 className="text-3xl text-center">
           Under construction... &#128679;
         </h1>
@@ -10,6 +12,13 @@ export default function UnderConstruction() {
           Pomogrids is a pomodoro timer with a calendar heat map, hoping to
           boost focus and accountability.{" "}
         </p>
+        <br />
+        <Image
+          src="/assets/pomogrids_homepage.png"
+          alt="pomogrids"
+          height="550"
+          width="550"
+        />
         <br />
         <p>Coming soon by the end of the month. Check us out then!</p>
       </div>
