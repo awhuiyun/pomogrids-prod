ab7eb7d1593f885cf38510132e070e91d8c1125e
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-03-14 17:52:49 +0800

    Edit initializeAdmin function

--- src/server/firebase_admin/index.ts
+++ src/server/firebase_admin/index.ts
@@ -1,15 +1,19 @@
 import * as admin from "firebase-admin";
 
 export default async function initializeAdmin() {
-  if (admin.apps.length > 0) {
-    return admin.app();
-  }
+  try {
+    if (admin.apps.length > 0) {
+      return admin.app();
+    }
 
-  return admin.initializeApp({
-    credential: admin.credential.cert({
-      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-    }),
-  });
+    return admin.initializeApp({
+      credential: admin.credential.cert({
+        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      }),
+    });
+  } catch (error) {
+    throw error;
+  }
 }
