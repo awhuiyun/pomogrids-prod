b51788cd31dc8f47807e1ffef2431c62a109eae0
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-03-23 17:12:55 +0800

    Remove setup page

--- src/pages/setup.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingSpinner from "@/components/LoadingSpinner";
-
-export default function SignInLandingPage() {
-  return (
-    <div className="mx-auto w-fit justify-center pt-20">
-      <LoadingSpinner />
-    </div>
-  );
-}
