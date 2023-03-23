ff06f8ed18cb762f245d72db4c87a7e38bbc0720
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-03-23 17:07:23 +0800

    Set up PremiumSuccessPage

--- src/pages/get-premium/success.tsx
+++ src/pages/get-premium/success.tsx
@@ -1,5 +1,40 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+import BaseButton from "@/components/BaseButton";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import useUserStore from "@/stores/user";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
 
-export default function GetPremiumSuccessPage() {
-  return <div>Thanks for your subscription!</div>;
+export default function PremiumSuccessPage() {
+  const router = useRouter();
+  const { user } = useUserStore();
+
+  if (!user) return <LoadingSpinner />;
+
+  return (
+    <div>
+      <div className="text-center">
+        <FontAwesomeIcon
+          icon={faStar}
+          className="animate-bounce text-amber-400 text-4xl"
+        />
+      </div>
+      <h1 className="mt-6 text-2xl font-bold text-center">
+        You are subscribed to the premium plan, hooray!!
+      </h1>
+      <h2 className="text-lg text-slate-400 text-center mt-4">
+        Thank you for the support! We hope this little thing we built helps you
+        out. Cheerrriiooss~
+      </h2>
+      <div className="text-center mt-10">
+        <div onClick={() => router.push("/")}>
+          <BaseButton
+            type="button"
+            label="Back to my tasks ->"
+            className="text-blue4 hover:underline underline-offset-2"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
